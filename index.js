360844c502717b59dbb7ada2aad4cceed1085f18
arpangohe <arpangohe02@gmail.com>
2024-07-19 16:44:49 +0530

    Change image

---
+++ index.js
@@ -0,0 +1,6 @@
+function changeImage(){
+    let store = document.querySelector('.iframe');
+    store.src = "https://youtu.be/Iy3Yw2zm7PY?si=bte44f75DJ8LcZGe";
+    store.width = "800";
+    store.height = "800";
+}
