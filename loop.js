0910e0893fdcf2c7c7baa566e81cdadfdaba22ea
arpangohe <arpangohe02@gmail.com>
2024-07-19 16:40:15 +0530

    LOOP

---
+++ loop.js
@@ -0,0 +1,22 @@
+let arr = [56,9,4,5,8];
+// let i;
+// for(let i in arr){
+//     console.log(i)
+//     console.log(arr[i])
+// }
+// for (let i in arr){
+//     console.log(i);
+
+// }
+
+// console.log(arr);
+// arr.pop();
+// arr.push(45)
+// console.log(arr);
+
+// arr.splice(arr.length-1,0,32);
+// console.log(arr)
+
+arr.splice(2,0,"Arpan");
+arr.splice(2,1);
+console.log(arr)
