7b22207b5b8d625057c92658f55fcf9e5594cbf0
arpangohe <arpangohe02@gmail.com>
2024-07-19 16:44:03 +0530

    day 1

---
+++ day1.js
@@ -0,0 +1,29 @@
+// function home(){
+//   // window.location.href = "https://www.google.com";
+
+//   //let h = window.innerHeight;
+//   //let w = window.innerWidth
+
+//   console.log(h,w)
+// }
+
+// function windowexample(){
+//   let number1=window.prompt("Enter Number 1");
+//   let number2=window.prompt("Enter Number 2");
+//   window.alert(number1+number2);
+// }
+
+// function windowexample(){
+//   let number1=parseInt(window.prompt("Enter Number 1"));
+//   let number2=parseInt(window.prompt("Enter Number 2"));
+//   window.alert(number1+number2);
+// }
+function windowexample(){
+  let username=window.prompt("Enter Username");
+  let password=window.prompt("Enter Password");
+  window.alert(username+"\n"+password);
+
+  if(username){
+    
+  }
+}
