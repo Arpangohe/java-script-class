bccfe7b648d213eed8de63b333c048a127573913
arpangohe <arpangohe02@gmail.com>
2024-07-22 17:31:24 +0530

    methods in array

---
+++ methods1.js
@@ -0,0 +1,21 @@
+// let arr = [34,57,34,45,253,543,4]
+// let arr1 = [12,123,23,313,33,32]
+
+// arr.reverse();
+
+// console.log(arr);
+// let output = arr.join("arpan")
+// console.log(output);
+
+// let output1 = arr.toString();
+// console.log(output1);
+
+// let arr3 = arr.concat(arr1)
+// console.log(arr3);
+
+// let output3 = arr.slice(1,3);
+// console.log(output3);
+function store(){
+    let arr = [12,312,312,21,212,3,32,3]
+window.localStorage.setItem('information',"Arpan")
+}
