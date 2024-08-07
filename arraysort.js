836da6e84382fcb05f8fbfd0162eabb26611b2ac
arpangohe <arpangohe02@gmail.com>
2024-08-07 16:33:21 +0530

    array sort

---
+++ arraysort.js
@@ -0,0 +1,11 @@
+let alphabetArray = ['d', 'a', 'c', 'b', 'e'];
+
+// Sort the array in alphabetical order
+alphabetArray.sort();
+
+// Display the sorted array on the screen
+console.log("Alphabet Array in Alphabetical Order:");
+console.log(alphabetArray);
+
+// If you want to display it on the HTML page, you can use the following code:
+// document.write("Alphabet Array in Alphabetical Order: " + alphabetArray);
