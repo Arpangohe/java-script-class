b8a42a5e89d6b673b6004e74f2042b706e1b6c36
arpangohe <arpangohe02@gmail.com>
2024-08-07 16:27:06 +0530

    Import & Export Module

---
+++ export.js
@@ -0,0 +1,19 @@
+// let num1 = 56;
+// let num2 = 7;
+
+// const fun = ()=>{
+    //console.log("This is function call");  
+// }
+
+// named export
+
+// export {num1,num2,fun};
+
+let str = "This is string";
+
+let run = ()=>{
+    console.log("This is funtion run");
+}
+
+export default str;
+export {run};
---
+++ import.js
@@ -0,0 +1,13 @@
+// import { num1, num2, fun } from "./export.js";
+// console.log(num1, num2);
+// console.log(fun);
+
+// named import
+
+//default import
+
+import index from './export.js';
+import {run} from './export.js'
+
+console.log(index);
+run();
